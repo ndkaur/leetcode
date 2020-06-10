d3d66dbde5a4780e0be1586243ac1166af08140f
nirmal deep kaur <ndkaurn@gmail.com>
2020-06-10 22:40:58 +0530

    another easy sol for 605

--- 605.can-place-flowers.cpp
+++ 605.can-place-flowers.cpp
@@ -15,44 +15,69 @@ void print(vi &out){
 }
 
 // @lc code=start
+// class Solution {
+// public:
+//     bool canPlaceFlowers(vector<int>& flowerbed, int n){    
+//         int len = flowerbed.size();
+//         int count=0;
+//         // if(len==1 && flowerbed[0]==0)
+//         //     return true;
+//         for(int i=0;i<len;i++){
+//             if(flowerbed[i]==0){
+//                 // starting i from [i]=0 
+//                 if(i==0 && flowerbed[i+1]!=1)
+//                 {
+//                     flowerbed[i]=1;
+//                     count++;
+//                 }  
+//                 // last value of i is 0;
+//                 else if(i==len-1 && flowerbed[i-1]!=1)
+//                 {
+//                     flowerbed[i]=1;
+//                     count++;
+//                 }
+//                 //  when i is in between the array
+//                 else if(i!=0 && i!=flowerbed.size()-1 && flowerbed[i+1]!=1 && flowerbed[i-1]!=1)
+//                 {
+//                     flowerbed[i]=1;
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count>=n;
+//     }
+// };
+
+// solving with an esy method
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n){    
-        int len = flowerbed.size();
+    bool canPlaceFlowers(vector<int>& flower, int n){    
+        int len = flower.size();
         int count=0;
-        // if(len==1 && flowerbed[0]==0)
-        //     return true;
         for(int i=0;i<len;i++){
-            if(flowerbed[i]==0){
-                // starting i from [i]=0 
-                if(i==0 && flowerbed[i+1]!=1)
-                {
-                    flowerbed[i]=1;
+            if(flower[i]==0){
+                bool valid=true;
+                if(i-1>=0 && flower[i-1])
+                    valid=false;
+                if(i+1<len && flower[i+1])
+                    valid=false;
+                if(valid) {
                     count++;
-                }  
-                // last value of i is 0;
-                else if(i==len-1 && flowerbed[i-1]!=1)
-                {
-                    flowerbed[i]=1;
-                    count++;
-                }
-                //  when i is in between the array
-                else if(i!=0 && i!=flowerbed.size()-1 && flowerbed[i+1]!=1 && flowerbed[i-1]!=1)
-                {
-                    flowerbed[i]=1;
-                    count++;
+                    flower[i]=1;
                 }
             }
         }
-        return count>=n;
+        return (count>=n);
     }
 };
+        
+
 
 int main(){
     Solution sol;
     vector<int> flower={1,0,0,0,1};
     int n;
-    bool out=sol.canPlaceFlowers(flower,2);
+    bool out=sol.canPlaceFlowers(flower,1);
     cout<<out;
     return 0;
 }
