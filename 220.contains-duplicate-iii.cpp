a621a9d2eac5f656b778371c742cc04c0f57f950
nirmal deep kaur <ndkaurn@gmail.com>
2020-09-03 10:26:11 +0530

    lc

---
+++ 220.contains-duplicate-iii.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=220 lang=cpp
+ *
+ * [220] Contains Duplicate III
+ */
+#include "bits/stdc++.h"
+using namespace std;
+#include "Tree.h"
+#define deb(x) cout<<x<<endl;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef pair<int,int> pii;
+#include "LinkedList.h"
+
+void print(vi &out){
+    for(auto x: out) cout<<x<<" ";
+    cout<<endl;
+}
+
+// @lc code=start
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        int n=nums.size();
+        vector<pair<int,int>> v(n);
+        for(int i=0;i<n;i++){
+            v[i].first=nums[i];
+            v[i].second=i;
+        }
+        sort(v.begin(),v.end());
+    
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(v[i].first+t>=v[j].first){
+                    if(abs(v[i].second-v[j].second)<=k){
+                        return true;
+                    }
+                }
+                else
+                    break;
+            }
+        }
+        return false;
+    }
+};
+// @lc code=end
+
+
+int main(){
+    Solution sol;
+    vector<int> nums={1,0,1,1};
+    int t=2;
+    int k=1;
+    bool ans=sol.containsNearbyAlmostDuplicate(nums,k,t);
+    cout<<ans;
+    return 0;
+}
