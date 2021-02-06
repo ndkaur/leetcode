6f9198641e460e53a5a3c2e7af1582973b23476d
nirmal deep kaur <ndkaurn@gmail.com>
2021-02-06 09:41:01 +0530

    lc

--- 783.minimum-distance-between-bst-nodes.cpp
+++ 783.minimum-distance-between-bst-nodes.cpp
@@ -1,7 +1,7 @@
 /*
- * @lc app=leetcode id=938 lang=cpp
+ * @lc app=leetcode id=783 lang=cpp
  *
- * [938] Range Sum of BST
+ * [783] Minimum Distance Between BST Nodes
  */
 #include "bits/stdc++.h"
 using namespace std;
@@ -18,7 +18,6 @@ void print(vi &out){
     for(auto x: out) cout<<x<<" ";
     cout<<endl;
 }
-
 // @lc code=start
 /**
  * Definition for a binary tree node.
@@ -33,25 +32,27 @@ void print(vi &out){
  */
 class Solution {
 public:
-    int rangeSumBST(TreeNode* root, int low, int high) {
-        int sum=0;
-        dfs(root,low,high, sum);
-        return sum;
-    }
-    void dfs(TreeNode* root , int low, int high , int &sum ){
+    int prev=-1;
+     int ans=INT_MAX;
+    int minDiffInBST(TreeNode* root) {
         if(!root)
-            return ;
-        if(root->val>=low && root->val<=high)
-            sum+=root->val;
-        dfs(root->left ,low, high, sum);
-        dfs(root->right , low, high, sum);
+            return 0;
+       // left root right
+        if(root->left) 
+            minDiffInBST(root->left);
+        if(prev>=0) {
+            int curans= root->val-prev;
+            ans= min(ans,curans);
+        }
+        prev=root->val;
+        if(root->right)
+            minDiffInBST(root->right);
+        return ans;
     }
 };
 // @lc code=end
 
-
 int main(){
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     
     return 0;
 }
