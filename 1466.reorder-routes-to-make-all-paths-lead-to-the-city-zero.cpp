8a7a0492376331641887ac49744da387f7c2c84c
nirmal deep kaur <ndkaurn@gmail.com>
2021-12-15 16:12:51 +0530

    graph

--- 1466.reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466.reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -21,16 +21,78 @@ cout<<endl;
 }
 
 // @lc code=start
-class Solution {
+class Solution0 {
 public:
-    int minReorder(int n, vector<vector<int>>& connections) {
-        
+    int minReorder(int n, vector<vector<int>>& c) {
+        int sz= c.size();
+        vector<pair<int,int>> adj[n];
+        for(int i=0;i<sz;i++){
+            int u= c[i][0];
+            int v= c[i][1];
+            adj[u].push_back({v,1});
+            adj[v].push_back({u,0});
+        }
+        int ans =0;
+        vector<bool> visited(n,false);
+        dfs(0,ans,visited,adj);
+        return ans;
+    }
+    void dfs(int node, int &ans , vector<bool>& visited,vector<pair<int,int>> adj[]){
+        visited[node]=true;
+        for(auto pir:adj[node]){
+            int nf= pir.first;
+            int weight= pir.second;
+            if(visited[nf]==false){
+                ans+=weight;
+                dfs(nf,ans, visited, adj);
+            }
+        }
     }
 };
+
+// adj list ->
+// 0-> [1,1],[4,0]
+// 1-> [0,0],[3,1]
+// 2 -> [3,1]
+// 3-> [1,0],[2,0]
+// 4-> [5,1],[0,1]
+// 5-> [4,0]
+
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& c) {
+        vector<bool> visited(n,false);
+        visited[0]=true;
+        int inverse=0;
+        int i=0;
+        while(i<c.size()){
+            if(visited[c[i][1]]){
+                // if visited value is on right side 
+                // [2,0] we want 0 on right side 
+                visited[c[i][0]]=true; // mark other value as seen
+            }
+            else if(visited[c[i][0]]){
+                // if visited value is on left side need for path change 
+                inverse++;
+                visited[c[i][1]]=true;
+            }
+            else {
+                c.push_back(c[i]); // if none of the value is seen then add the whole pair at last of connections 
+            }
+            i++;
+        }
+        return inverse;
+    }
+}; 
+
 // @lc code=end
 
 
 int main(){
-
-   return 0;
+    Solution sol;
+    vector<vector<int>> con ={{0,1},{1,3},{2,3},{4,0},{4,5}};
+    int n= 6;
+    int ans = sol.minReorder(n,con);
+    cout<<ans;
+    return 0;
 }
