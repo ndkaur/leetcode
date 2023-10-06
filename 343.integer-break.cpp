6aa64bd174710dbcef5fde3054708ff75eece511
ndkaur <ndkaurn@gmail.com>
2023-10-06 20:47:05 +0530

    lc

--- 343.integer-break.cpp
+++ 343.integer-break.cpp
@@ -35,6 +35,47 @@ public:
         return product;
     }
 };
+
+// using dp 
+// so for num = 1 , we cant break ans = 1
+// for num =2 , 1+1 = 1*1 = 1 
+// num = 3, 2+1 = 1*2 = 2 max 
+// for num =4 => (1+3) ,(2+2) , (3+1) 
+// further breaking each pair ->  there are 4 possibilities for each pair (a,b)
+// 1. we cant break both so take their product -> a*b
+
+// 2. we can break a , so we need max value of a which is stored in dp 
+//   so dp[a] * b 
+
+// 3. we can break b,so take max values of so a * dp[b]
+
+// 4. we can break both a and b , dp[a]*dp[b]
+
+// simply taking what its values in the dp 
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int>dp(100);
+        dp[1]=1;
+        dp[2]=1; 
+        dp[3]=2;
+        for(int i=4; i<=n; i++){
+            //breaking the num 
+            // eg = 4 -> (1,3) (2,2) (3,1)
+            int num = i; // num=4
+            for(int j=1; j<=num; j++){
+                int a = j;
+                int b = num-a; // 4-1=3, 4-2=2 , 4-3=1
+                // no taking maximum of all 4 cases 
+                dp[i] = max({dp[i], a*b , dp[a]*b, a*dp[b],  dp[a]*dp[b]});
+            }
+        }
+        return dp[n];
+    }
+};
+
+
 // @lc code=end
 
 
