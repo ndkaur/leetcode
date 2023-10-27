c4a67f96fb10fb8512538df2f8c1a076b02736ff
ndkaur <ndkaurn@gmail.com>
2023-10-27 10:37:47 +0530

    lc

--- 823.binary-trees-with-factors.cpp
+++ 823.binary-trees-with-factors.cpp
@@ -22,6 +22,29 @@ void print(vi &out){
 
 // @lc code=start
 
+
+class Solution {
+public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        int n = arr.size();
+        long ans = 0;
+        long mod = 1e9+7;
+        sort(arr.begin(), arr.end());
+        // used map cause we eant to find what is present at the exact location required
+        unordered_map<int,long> dp;
+        for(int i=0; i<n; i++){
+            dp[arr[i]] = 1;
+            for(int j=0; j<i; j++){
+                if(arr[i]%arr[j]==0){
+                    dp[arr[i]] = (dp[arr[i]] + dp[arr[j]] * dp[arr[i]/arr[j]])%mod;
+                }
+            }
+            ans = (ans + dp[arr[i]]) % mod;
+        }
+        return ans;
+    }
+};
+
 class Solution0 {
     int mod = 1e9 +7;
 public:
