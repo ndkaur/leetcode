0d3434bf1c29714ea4f8eee5d50c458449207db1
ndkaur <ndkaurn@gmail.com>
2023-12-07 10:43:34 +0530

    lc

--- 1903.largest-odd-number-in-string.cpp
+++ 1903.largest-odd-number-in-string.cpp
@@ -35,6 +35,27 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int n = num.size();
+        for(int i=n-1; i>=0; i--){
+            if((num[i]-'0')%2 !=0){
+              return num.substr(0,i+1);
+          }
+        }
+        return "";
+    }
+};
+
+// 3 5 4 2 7 
+// last digit 7 is odd so full number is odd return it 
+// eg -> 353427
+// full num i odd but if num = 353422
+// then we reach till 3 no three is odd it will make remaining number odd
+// so ans = substring from starting to that i 
+
 // @lc code=end
 
 
