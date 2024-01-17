84ad96a8bfe8b87491cd30bda8b4f7135ec5285b
ndkaur <ndkaurn@gmail.com>
2024-01-17 11:27:38 +0530

    lc

--- 2110.Number-of-Smooth-Descent-Periods-of-a-Stock.cpp
+++ 2110.Number-of-Smooth-Descent-Periods-of-a-Stock.cpp
@@ -36,6 +36,32 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        int n = prices.size();
+        int idx = 0; // the index where condtion wasnt valid 
+        long long cnt = 1;
+        
+        for(int i=1; i<n; i++){
+            if(prices[i]+1!=prices[i-1]){
+                idx = i;
+                cnt++;
+            }
+            else // equal , condition valid
+                cnt+= i-idx+1;
+        }
+        return cnt;
+    }
+};
+
+// i = 1 , 3==2+1 so cnt = 1+ (1-0)+1 = 3=cnt 
+// i = 2, 2 =1+1 so cnt = 3 + (2-0)+1 = 3+2+1=6
+// i= 3, 3 !=1+1  so idx = 3, cnt++ = cnt=7
+
+
+
 int main(){
      Solution sol;
      vector<int> prices={3,2,1,4};
--- 647.palindromic-substrings.cpp
+++ 647.palindromic-substrings.cpp
@@ -41,6 +41,34 @@ public:
     }
 };
 
+class Solution1 { //O(n^2)
+public:
+    int check(string& s, int i, int j, vector<vector<int>>& dp){
+        if(i>=j)
+            return 1;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int ans = 0;
+        if(s[i]==s[j])
+            ans = check(s, i+1, j-1, dp);
+        else 
+            ans = 0;
+        return dp[i][j] = ans;
+    }
+    int countSubstrings(string s) {
+        int n = s.size();
+        int cnt = 0;
+        
+        vector<vector<int>> dp(n, vector<int>(n,-1));
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                cnt += check(s,i,j,dp);
+            }
+        }
+        return cnt;
+    }
+};
+
 class Solution1 {
 public:
     int countSubstrings(string s) {
